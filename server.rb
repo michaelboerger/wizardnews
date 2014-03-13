637fd9030b053a8799b60b969cc26bedd83f17dd
Robel <robeltadesse01@gmail.com>
2014-03-13 11:18:54 -0400

    2.0

--- server.rb
+++ server.rb
@@ -1,32 +1,36 @@
 require 'sinatra'
 require 'shotgun'
 require 'csv'
+require 'pry'
 
 get '/'  do
   @wizards=[]
-   CSV.foreach('wizards.csv') do |file|
-    @wizards<<file.to_s
+   CSV.foreach('wizards.csv', headers: true) do |file|
+    @wizards << file[0].to_s
+    @wizards << file[1].to_s
+    @wizards << file[2].to_s
    end
-  #read in CSV
+  # read in CSV
   erb :index
 end
 
 get '/new-wizard-article'  do
+
   erb :new_wizard_article
 end
 
 post '/new-wizard-article' do
-  @title=[]
-  @url=[]
-  @description=[]
-  @title<<params['title']
-  @url<<params['url']
-  @description<<params['description']
 
-  CSV.open('wizards.csv', 'wb') do |file|
-      file<<@title
-      file<<@url
-      file<<@description
+  @title=params['title']
+  @url=params['url']
+  @description=params['description']
+
+  CSV.open('wizards.csv', 'ab') do |file|
+
+      file << [@title, @url, @description]
+      # file<<@title
+      # file<<@url
+      # file<<@description
   end
   redirect '/'
 end
